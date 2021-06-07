f3cea1625aaabf9420d57dbbbe76b730c52db749
Mayan Lm Kumar <mayankmr2@gmail.com>
2021-06-07 15:58:22 +0530

    stock spring package with dependencies
    Spring package with dependencies

---
+++ emprecords/src/main/java/com/mayank2/emprecords/EmprecordsApplication.java
@@ -0,0 +1,13 @@
+package com.mayank2.emprecords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmprecordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmprecordsApplication.class, args);
+	}
+
+}
